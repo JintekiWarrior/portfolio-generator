bd3e569359fbeb8a3c92a2f6ced07259b69ad367
Moe Yassine <moeyassine1993@gmail.com>
2022-03-16 12:29:19 -0400

    Create html template with js - 9.3.1

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length)
-console.log(profileDataArgs)
+const fs = require('fs')
+const generatePage = require('./src/page-template')
 
-const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i])
-  }
-}
+const profileDataArgs = process.argv.slice(2)
+const [name, github] = profileDataArgs
 
-printProfileData(profileDataArgs)
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err
+
+  console.log("Portfolio complete! Checkout index.html")
+})
